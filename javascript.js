1a3318c82d203127f5870d9c5d7974947ec7e593
anniebete <44245741+anniebete@users.noreply.github.com>
2020-12-29 00:08:11 -0500

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -6,6 +6,6 @@ var links = [
 
 function randomLink() {
   var randomNumber = Math.floor(Math.random() * (links.length));
-  document.getElementsByTagName("h2").innerHTML = links[randomNumber];
+  document.getElementsById("link").href = links[randomNumber];
 }
 
