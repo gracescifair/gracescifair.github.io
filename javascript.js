449b91f362614cfdd5fa915914d1d978dffd47f6
anniebete <44245741+anniebete@users.noreply.github.com>
2020-12-28 22:57:14 -0500

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,1 +1,11 @@
+var links = [
+  'https://gracescifair.github.io/survey1',
+  'https://gracescifair.github.io',
+  'https://gracescifair.github.io'
+]
+
+function randomLink() {
+  var randomNumber = Math.floor(Math.random() * (links.length));
+  documents.getElementById('link').innerHTML = links[randomNumber];
+}
 
