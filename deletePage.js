a323a81db377e63ce715ba0ac25641eb665ea5d2
anniebete <44245741+anniebete@users.noreply.github.com>
2021-01-01 16:00:01 -0500

    Delete deletePage.js

--- deletePage.js
+++
@@ -1,7 +0,0 @@
-function eraseContent() {
-  
-    document.getElementById("first").innerHTML = "";
-    document.getElementById("second").innerHTML = "";
-    document.getElementById("third").innerHTML = "";
-  
-}
