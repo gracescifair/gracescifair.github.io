5994fbd033e9a80d9dc1e26364f5bd85b4b58015
anniebete <44245741+anniebete@users.noreply.github.com>
2021-01-01 16:02:36 -0500

    Update sendToPage.js

--- sendToPage.js
+++ sendToPage.js
@@ -1,7 +1,7 @@
 var links = [
-  'survey1',
-  'survey2',
-  'survey3'
+  'https://docs.google.com/forms/d/e/1FAIpQLSc0R6Drq0GDj6g3jK1LGlyXEpviKeXcT_XigunqIcNnvQVLeQ/viewform?usp=sf_link',
+  'https://docs.google.com/forms/d/e/1FAIpQLScR8YMeGLGIwT4ZpQIXcBozHb4sKvSR19bwBxp_dHUj4GWeOg/viewform?usp=sf_link',
+  'https://docs.google.com/forms/d/e/1FAIpQLSf8gIV7S3FzHtW9BDokd6SNl-VqA_-B1jXWXh8jQx2wj26fOQ/viewform?usp=sf_link'
 ]
 
 function randomLink() {
