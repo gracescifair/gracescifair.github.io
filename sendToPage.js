709f941180b75082f4e7aa3d38761ed5a636613e
anniebete <44245741+anniebete@users.noreply.github.com>
2021-11-04 23:45:51 -0400

    Update sendToPage.js

--- sendToPage.js
+++ sendToPage.js
@@ -1,6 +1,6 @@
 var links = [
-  'https://osu.az1.qualtrics.com/jfe/form/SV_8pNI05SIvRnP3W6',
-  'https://osu.az1.qualtrics.com/jfe/form/SV_6DrQCK5cxX8Rnx4'
+  'https://osu.az1.qualtrics.com/jfe/form/SV_6DrQCK5cxX8Rnx4',
+  'https://osu.az1.qualtrics.com/jfe/form/SV_8pNI05SIvRnP3W6'
 ]
 
 function randomLink() {
